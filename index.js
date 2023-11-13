7a48c20fd3cad29da417b9d7b58420a0eed88a2f
EA - Vinicius Santos Paixão Barbosa <vinicius_barbosa@educadventista.org>
2023-11-13 15:02:02 -0300

    recebendo informações do formulario no corpo da requisição

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express")
 const path = require("path")
 
+const app =express()
+
 const caminhoBase = path.join(__dirname, "templates")
 
-const app =express()
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadatrar/salvar', (requisicao, resposta) => {
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`o email do usuario é: ${email}`)
+})
+
+app.get('/cadastrar', (requisicao, resposta) =>{
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
 
 
 app.get('/usuarios/:id', (requisicao, resposta) => {
